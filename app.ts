12b3405d5df61400767697fdbcc1280fc4703c01
Javier Amaya <Javieramayapat@gmail.com>
2021-01-02 13:25:05 -0600

    Templates Literales

--- app.ts
+++ app.ts
@@ -1,6 +1,34 @@
 // Creación de una función anonima auto invocada en javascript
 (function(){
 
+    /* 
+        Template Literales:Nos permiten construir string de manera más
+                           sencilla.
+
+        Para imprimir una variable dentro de un template literal
+        debo encerrarla dentro de ${ nombre } 
+
+        Nota: Los templates literales permiten el procesamiento
+              de infromación debido a que entre ${} las llaves
+              corre codigo navito de javascript
+    
+    */
+
+    const nombre = 'Javier';
+    const apellido = 'Amaya';
+    const edad = 24;
+
+    const sumar = () => edad + 5;
+
+    // Formar la siguientes salida  -> Javier Amaya (24)
+    // const salida = nombre + " " + apellido + "(" + edad + ")";
+    
+    const salida = `${nombre} ${apellido} (${sumar()})`;
+
+  
+    
+    console.log(salida);
+    
   
 })();
 
--- typescript/template-literal.ts
+++ typescript/template-literal.ts
@@ -1,24 +1,36 @@
-"use strict";
 // Creación de una función anonima auto invocada en javascript
-(function () {
-    /*
+(function(){
+
+     /* 
         Template Literales:Nos permiten construir string de manera más
                            sencilla.
 
         Para imprimir una variable dentro de un template literal
-        debo encerrarla dentro de ${ nombre }
+        debo encerrarla dentro de ${ nombre } 
 
         Nota: Los templates literales permiten el procesamiento
               de infromación debido a que entre ${} las llaves
               corre codigo navito de javascript
     
     */
-    var nombre = 'Javier';
-    var apellido = 'Amaya';
-    var edad = 24;
-    var sumar = function () { return edad + 5; };
-    // Formar la siguientes salida  -> Javier Amaya (24)
-    // const salida = nombre + " " + apellido + "(" + edad + ")";
-    var salida = nombre + " " + apellido + " (" + sumar() + ")";
-    console.log(salida);
+
+   const nombre = 'Javier';
+   const apellido = 'Amaya';
+   const edad = 24;
+
+   const sumar = () => edad + 5;
+
+   // Formar la siguientes salida  -> Javier Amaya (24)
+   // const salida = nombre + " " + apellido + "(" + edad + ")";
+   
+   const salida = `${nombre} ${apellido} (${sumar()})`;
+
+ 
+   
+   console.log(salida);
+   
+  
 })();
+
+
+
