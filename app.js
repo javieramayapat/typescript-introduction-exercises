fb042b75fa04e0173cdf22e7e3885051368f3812
Javier Amaya <Javieramayapat@gmail.com>
2021-01-02 12:06:52 -0600

    Variables Let y Const

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 "use strict";
 // Creación de una función anonima auto invocada en javascript
 (function () {
-    function saludar(nombre) {
-        console.table('Hola ' + nombre); // Hola Logan
+    /*
+        Let: Puede perfecamento sutituir a var para poder almacenar
+             espacio de memoria para una variable
+        let: Solo vive en el espacio delimitado por las llaves llamado scope
+        Nota: No permite redeclarar una variable en el mismo scope.
+
+        const: Me sirve para declarar constantes en typescript
+        Nota: Las constantes pesan menos en la memorias si el valor
+              no va a cambiar
+        Las constantes en mayusculas indican que una variable nunca  va
+        a cambiar de valor.
+
+    */
+    var mensaje = 'hola';
+    var HOLA = 'Hola Javier';
+    hola = 123;
+    if (true) {
+        var mensaje_1 = 'mundo';
     }
-    var wolverine = {
-        nombre: 'Logan'
-    };
-    saludar(wolverine.nombre);
+    console.log(mensaje);
 })();
--- app.ts
+++ app.ts
@@ -1,17 +1,29 @@
 // Creación de una función anonima auto invocada en javascript
 (function(){
 
-    function saludar( nombre:string ) {
-        console.table( 'Hola ' + nombre ); // Hola Logan
+    /* 
+        Let: Puede perfecamento sutituir a var para poder almacenar
+             espacio de memoria para una variable
+        let: Solo vive en el espacio delimitado por las llaves llamado scope
+        Nota: No permite redeclarar una variable en el mismo scope.
+
+        const: Me sirve para declarar constantes en typescript
+        Nota: Las constantes pesan menos en la memorias si el valor
+              no va a cambiar
+        Las constantes en mayusculas indican que una variable nunca  va
+        a cambiar de valor.
+
+    */
+
+    let mensaje = 'hola';
+    const HOLA = 'Hola Javier';
+    hola = 123;
+
+    if(true){
+        let mensaje = 'mundo';
     }
-    
-    
-    const wolverine = {
-        nombre: 'Logan'
-    };
-    
-    
-    saludar( wolverine.nombre );
+
+    console.log(mensaje);
 
 })();
 
